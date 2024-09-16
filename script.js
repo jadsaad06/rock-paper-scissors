d5ff601481c042c167303f56938323902e553a95
Jad Saad <jadsaad06@gmail.com>
2024-09-15 23:35:56 -0700

    Fixed scoreboard and finished up project

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function getComputerChoice() {
+const humanChoicePic = document.querySelector(".option-UI#playerChoice");
+const computerChoicePic = document.querySelector(".option-UI#compChoice");
+
+const changeChoicePic = function(player, choice){
+    if(choice == "scissors"){
+        player.src = "https://png.pngtree.com/png-vector/20220706/ourmid/pngtree-cute-red-scissors-clipart-png-image_5720189.png";
+    }else if(choice == "paper"){
+        player.src = "https://spaces-cdn.clipsafari.com/d6xub7hw3shnvf3zp7a0yg32f9xg";
+    }else{
+        player.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHmwUZx2AG6bjbC5VrglrRW6M1EQ-9zX_Mwq34TMcCnKGyTaOYrDHvSm1uc_G4Xs-FBHM&usqp=CAU";
+    }
+}
+
+
+const getComputerChoice = function () {
     let randChoice = Math.random();
     let compChoice = "";
     
@@ -14,7 +28,7 @@ function getComputerChoice() {
 }
 
 
-function getHumanChoice(){
+const getHumanChoice = function(){
     let choice = prompt("Rock, Paper, or Scissors?");
 
     return choice.toLowerCase();
@@ -22,41 +36,46 @@ function getHumanChoice(){
 
 let humanScore = 0;
 let computerScore = 0;
+const scoreboard = querySelector("#scoreboard");
+const declareWinner = querySelector("#declare-winner");
 
-function playRound(humanChoice, computerChoice){
+const playRound = function (humanChoice, computerChoice){
+    changeChoicePic(humanChoicePic, humanChoice);
+    changeChoicePic(computerChoicePic, computerChoice);
+
     if(humanChoice == "rock"){
         if(computerChoice == "paper"){
-            alert("You lose! Paper beats Rock");
+            declareWinner.value ="You lose! Paper beats Rock";
             computerScore++;
         }else if(computerChoice == "scissors"){
-            alert("You win! Rock beats Scissors");
+            declareWinner.value ="You win! Rock beats Scissors";
             humanScore++;
         }else{
-            alert("Tie! Computer chose Rock as well!");
+            declareWinner.value ="Tie! Computer chose Rock as well!";
         }
     }else if (humanChoice == "paper"){
         if(computerChoice == "scissors"){
-            alert("You lose! Scissors beats Paper");
+            declareWinner.value ="You lose! Scissors beats Paper";
             computerScore++;
         }else if(computerChoice == "rock"){
-            alert("You win! Paper beats Rock");
+            declareWinner.value ="You win! Paper beats Rock";
             humanScore++;
         }else{
-            alert("Tie! Computer chose Paper as well!");
+            declareWinner.value ="Tie! Computer chose Paper as well!";
         }
     }else{
         if(computerChoice == "rock"){
-            alert("You lose! Rock beats Scissors");
+            declareWinner.value ="You lose! Rock beats Scissors";
             computerScore++;
         }else if(computerChoice == "paper"){
-            alert("You win! Scissors beats Paper");
+            declareWinner.value ="You win! Scissors beats Paper";
             humanScore++;
         }else{
-            alert("Tie! Computer chose Scissors as well!");
+            declareWinner.value ="Tie! Computer chose Scissors as well!";
         }
     }
 
-    alert(`Human Score: ${humanScore}\nComputer Score: ${computerScore}`)
+    scoreboard.value =`Human Score: ${humanScore}\nComputer Score: ${computerScore}`;
 }
 
 
@@ -66,4 +85,4 @@ function playRound(humanChoice, computerChoice){
 // const computerChoice = getComputerChoice();
 // playRound(humanChoice, computerChoice);
 
-// alert("Thanks for Playing!");
+// declareWinner.value ="Thanks for Playing!");
